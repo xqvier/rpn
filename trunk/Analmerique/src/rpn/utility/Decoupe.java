0004610d5cc707bcd82f0785c7d607e910222efc
xqvier@gmail.com <xqvier@gmail.com@fd78dd3b-d671-4a1a-eb6b-060b6dc42ec9>
2012-10-23 14:13:19 +0000

    Attention, grosse verge en approche!!
    DONT GET SODOMIZED !!!

---
+++ trunk/Analmerique/src/rpn/utility/Decoupe.java
@@ -0,0 +1,47 @@
+package rpn.utility;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Decoupe {
+
+	public static Queue<Element> decoupe(String pEquation) {
+		String equation = pEquation;
+		LinkedList<Element> equationTraitee = new LinkedList<Element>();
+		Pattern pattern = Pattern.compile("[0-9]+|[-+*/]|[()]");
+		Matcher matcher = pattern.matcher(equation);
+		/*
+		 * Tant que l'on trouve des "morceaux" de notre équation, on instancie
+		 * en opérateur ou en opérande Ces Operateur et Operande sont ensuite
+		 * stocké dans une file, c'est sur cette file que l'on va appliquer
+		 * l'algorithme de transformation en notation polonaise inversée
+		 */
+		while (matcher.find())
+			System.out.println("" + matcher.group(0));
+		if (Decoupe.isNumber(matcher.group(0))) {
+			equationTraitee.add(new Operande(Double.valueOf(matcher.group(0))));
+		} else {
+			equationTraitee.add(new Operateur(matcher.group(0)));
+		}
+		return equationTraitee;
+	}
+
+	/**
+	 * Controle si l'argument est un nombre ou non
+	 * 
+	 * @param string
+	 *            String dont on veut vérifier si elle contient un nombre. La
+	 *            String peut contenir un point (Double)
+	 * @return true si la chaine est un nombre false sinon
+	 */
+	private static boolean isNumber(String string) {
+		for (int i = 0; i < string.length(); i++) {
+			if (!Character.isDigit(string.charAt(i)) && string.charAt(i) != '.')
+				return false;
+		}
+		return true;
+	}
+
+}
