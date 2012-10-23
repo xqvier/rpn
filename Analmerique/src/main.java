49e4c7738b3b407614ac90e82ff6295a79f0551a
xqvier <xqvier@gmail.com>
2012-10-23 14:13:19 +0000

    Attention, grosse verge en approche!!
    DONT GET SODOMIZED !!!

---
+++ Analmerique/src/main.java
@@ -0,0 +1,10 @@
+
+class FirstApp {
+   public static void main (String[] args){
+    
+		String verge = " 256(54)+++++---***///%DSUUUUUUUUUUUUUUUCE";
+		
+		rpn.utility.Decoupe.decoupe(verge);
+	 
+   }
+}
